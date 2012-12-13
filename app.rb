71738c4ba1e654b5b39eb5277c61eeb525424de6
llosimura <alfonsoglezluis@gmail.com>
2012-12-13 20:40:31 +0000

    Ahora se  puede hacer paso a paso

--- app.rb
+++ app.rb
@@ -10,11 +10,10 @@ mao = nil
 mid = nil
 
 get '/' do
-  haml :actors
 end
 
 get '/service' do
-  haml :service, :locals => {:msg => "", :data => ""}
+  haml :service
 end
 
 post '/service' do
@@ -24,6 +23,7 @@ post '/service' do
     actors = data["actores"]
     objectives = data["objetivos"]
 
+
     #obtenemos los actores desde el JSON
     actors.each do |a|
        data_list = []
@@ -32,6 +32,7 @@ post '/service' do
        end
        actors_list << Actor.new(*data_list)
     end
+
 
     #obtenemos los objetivos desde el JSON
     objectives.each do |o|
@@ -44,17 +45,75 @@ post '/service' do
     #obtenemos la matriz MAO
     mao = data["2MAO"]
     mid = data["MID"]
-
-    haml :service, :locals => {:msg => "", :data => ""}
+    haml :service, :locals => {:msg => "Aqui te mostraria otros resultados!"}
   rescue  
     haml :service, :locals => {:msg => "El JSON era incorrecto", :data => params[:data]}
   end
 end
 
-=begin
-  Fase1: Creación de la lista de actores
-=end 
-post '/fase1' do
+get '/genera' do
+   haml :input
+end
+
+post '/genera' do
+  #Generamos el JSON actores
+  actorsJson = "{ \"actores\" : ["
+  params[:actors].each_pair do |id, properties|
+    actorsJson << "{"
+    properties.each_pair do |prop, val|
+      actorsJson <<"\"#{prop}\" :  \"#{val}\","
+    end
+    actorsJson << "},"
+  end
+  actorsJson << "],"
+  actorsJson.gsub!(",}","}");
+  actorsJson.gsub!("},]","}]");
+  puts actorsJson
+
+  #Generamos el JSON objetivos
+  objectivesJson = "\"objetivos\" : ["
+  params[:objectives].each_pair do |id, properties|
+  objectivesJson << "{"
+    properties.each_pair do |prop, val|
+      objectivesJson <<"\"#{prop}\" :  \"#{val}\","
+    end
+    objectivesJson << "},"
+  end
+  objectivesJson << "],"
+  objectivesJson.gsub!(",}","}");
+  objectivesJson.gsub!("},]","}]");
+  puts objectivesJson
+
+  #Generamos el JSON 2MAO
+  maoJson = "\"2MAO\" : ["
+  params[:mao].each_pair do |i,v|
+    maoJson << "["
+    v.each_pair do |j, valor|
+      maoJson << valor << ","
+    end
+    maoJson << "],"
+  end
+  maoJson << "],"
+  maoJson.gsub!(",]", ']')
+   
+   #Generamos el JSON midi
+   midiJson = "\"MIDI\" : ["
+   params[:midi].each_pair do |i,v|
+     midiJson << "["
+     v.each_pair do |j, valor|
+        midiJson << valor << ","
+     end
+     midiJson << "],"
+   end
+   midiJson << "]}"
+   midiJson.gsub!(",]", ']')
+
+   mactorJson = actorsJson + objectivesJson + maoJson + midiJson
+   haml :service, :locals => {:fromHelper => mactorJson }
+  
+ 
+end
+post '/actores' do
   prueba = params[:actors].clone
   prueba.each_pair do |id, properties|
     properties.each_pair do |prop, val|
@@ -63,24 +122,3 @@ post '/fase1' do
   end
 end
 
-=begin
-   Fase2: Creación de la lista de objetivos
-=end
-post '/fase2' do
-end
-
-
-post '/fase3' do
-end
-
-post '/fase4' do
-end
-
-post '/fase5' do
-end
-
-post '/fase6' do
-end
-
-post '/fase7' do
-end
